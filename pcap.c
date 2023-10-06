fab22e5d6fb57a3b9ac23493859c177189626b29
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2023-10-06 22:00:42 +0900

    Update pcap.c

--- pcap.c
+++ pcap.c
@@ -24,6 +24,7 @@ bool parse(Param* param, int argc, char* argv[]) {
 	return true;
 }
 
+//ethernet Header
 #pragma pack(push,1)
 struct ethheader{
     uint8_t ether_dmac[6];
@@ -32,6 +33,7 @@ struct ethheader{
 };
 #pragma pack(pop)
 
+//ip Header
 #pragma pack(push,1)
 struct ipheader{
     uint8_t Version : 4, headerLength : 4;
@@ -47,6 +49,7 @@ struct ipheader{
 };
 #pragma pack(pop)
 
+//tcp Header
 #pragma pack(push,1)
 struct tcpheader{
     uint16_t sport;
@@ -85,28 +88,31 @@ int main(int argc, char* argv[]) {
         u_char* data = (u_char*)(packet + 14 + 20 + 20);
         int datalength = header->caplen-( 14 + 20 + 20);
 
+        if(iph->ProtocolID == IPPROTO_TCP)
+        {
 
+            printf("<source Info>\n");
+            printf("source mac address : %02x:%02x:%02x:%02x:%02x:%02x\n", rth->ether_smac[0], rth->ether_smac[1], rth->ether_smac[2], rth->ether_smac[3], rth->ether_smac[4], rth->ether_smac[5]);
+            printf("source ip address : %d.%d.%d.%d\n", iph->sip[0], iph->sip[1], iph->sip[2], iph->sip[3]);
+            printf("source port : %d\n", ntohs(tcph->sport));
 
-        printf("<source Info>\n");
-        printf("source mac address : %02x:%02x:%02x:%02x:%02x:%02x\n", rth->ether_smac[0], rth->ether_smac[1], rth->ether_smac[2], rth->ether_smac[3], rth->ether_smac[4], rth->ether_smac[5]);
-        printf("source ip address : %d.%d.%d.%d\n", iph->sip[0], iph->sip[1], iph->sip[2], iph->sip[3]);
-        printf("source port : %d\n", ntohs(tcph->sport));
+            printf("<destination Info>\n");
+            printf("destination mac address : %02x:%02x:%02x:%02x:%02x:%02x\n", rth->ether_dmac[0], rth->ether_dmac[1], rth->ether_dmac[2] ,rth->ether_dmac[3] ,rth->ether_dmac[4] ,rth->ether_dmac[5]);
+            printf("destination ip address : %d.%d.%d.%d\n", iph->dip[0], iph->dip[1], iph->dip[2], iph->dip[3]);
+            printf("destination port : %d\n", ntohs(tcph->dport));
 
-        printf("<destination Info>\n");
-        printf("destination mac address : %02x:%02x:%02x:%02x:%02x:%02x\n", rth->ether_dmac[0], rth->ether_dmac[1], rth->ether_dmac[2] ,rth->ether_dmac[3] ,rth->ether_dmac[4] ,rth->ether_dmac[5]);
-        printf("destination ip address : %d.%d.%d.%d\n", iph->dip[0], iph->dip[1], iph->dip[2], iph->dip[3]);
-        printf("destination port : %d\n", ntohs(tcph->dport));
-
-        int print_length = datalength < 16 ? datalength : 16;
-        if (print_length > 0) {
-            printf("<Data Field>\n");
-            for (int i = 0; i < print_length; i++) {
-                printf("%02x ", data[i]);
+            int print_length = datalength < 16 ? datalength : 16;
+            if (print_length > 0)
+            {
+                printf("<Data Field>\n");
+                for (int i = 0; i < print_length; i++) {
+                    printf("%02x ", data[i]);
+                }
+                printf("\n\n");
             }
-            printf("\n\n");
         }
-
 	}
 
 	pcap_close(pcap);
+    return 0;
 }
