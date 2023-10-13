b3984b9aea9c09fc43a0c4fad5e4b6627b30676f
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2023-10-13 12:03:27 +0900

    Update pcap.c

--- pcap.c
+++ pcap.c
@@ -84,8 +84,7 @@ int main(int argc, char* argv[]) {
         struct ethheader* rth = packet;
         struct ipheader* iph = (packet + sizeof(struct ethheader));
         struct tcpheader* tcph = (packet + sizeof(struct ethheader) + sizeof(struct ipheader));
-        int data_length = ntohs(iph->TotalLength) - (iph->headerLength) - (tcph->tHLEN*4);
-        int max_data_length = data_length > 16 ? 16 : data_length;
+
 
         if(iph->ProtocolID == IPPROTO_TCP && ntohs(rth->ether_type) == 0x0800)
         {
@@ -95,19 +94,28 @@ int main(int argc, char* argv[]) {
             printf("source ip address : %d.%d.%d.%d\n", iph->sip[0], iph->sip[1], iph->sip[2], iph->sip[3]);
             printf("source port : %d\n", ntohs(tcph->sport));
             printf("\n");
+
             printf("<destination Info>\n");
             printf("destination mac address : %02x:%02x:%02x:%02x:%02x:%02x\n", rth->ether_dmac[0], rth->ether_dmac[1], rth->ether_dmac[2] ,rth->ether_dmac[3] ,rth->ether_dmac[4] ,rth->ether_dmac[5]);
             printf("destination ip address : %d.%d.%d.%d\n", iph->dip[0], iph->dip[1], iph->dip[2], iph->dip[3]);
             printf("destination port : %d\n", ntohs(tcph->dport));
             printf("\n");
+
+            /*Num Test
             printf("iphl : %d\n", iph->headerLength);
-            printf("iptotl %d\n", ntohs(iph->TotalLength));
+            printf("ntohs iptotl %d\n", ntohs(iph->TotalLength));
+            printf("iptotl %d\n", iph->TotalLength);
             printf("tcpHLEN : %d\n", tcph->tHLEN*4);
             printf("size of Total L :%d\n", sizeof(iph->TotalLength));
             printf("\n");
-            printf("<data field>\n");
+            */
 
-            for(int i=0 ; i <16 ; i++){
+            printf("<data field>\n");
+            int data_length = ntohs(iph->TotalLength) - (iph->headerLength*4) - (tcph->tHLEN*4);
+            if (data_length > 16)
+                data_length = 16;
+            printf("data_length : %d\n", data_length);
+            for(int i=0 ; i < data_length ; i++){
                 printf("%02x ", packet[sizeof(struct ethheader) + (iph->headerLength*4) + (tcph->tHLEN*4) + i]);
             }
             printf("\n");
